dc3fe341de54c5275bd3ff7571ac7fb6ea8a407f
Jirawat Zhou <17247zhou@gmail.com>
2021-04-17 22:14:36 +0700

    Added React Router and Bootstrap dependencies and Menu to Appjs

--- db-react/src/App.js
+++ db-react/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        <div>
+          <nav className="navbar navbar-expand navbar-dark bg-dark">
+            <a href="/" className="navbar-brand">
+              EventRegistration
+            </a>
+            <div className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to={"/event"} className="nav-link">
+                  Event
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to={"/customer"} className="nav-link">
+                  Customer
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to={"/organizer"} className="nav-link">
+                  Organizer
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to={"/review"} className="nav-link">
+                  Review
+                </Link>
+              </li>
+            </div>
+          </nav>
+        </div>
+    )
+  }
 }
 
 export default App;
