887ab12047ae6c9e7b9718fbaaea7573f85d1984
Jirawat Zhou <17247zhou@gmail.com>
2021-04-20 22:40:24 +0700

    Added Review ORM

--- src/main/java/com/eventregistration/application/controller/ReviewORM.java
+++ src/main/java/com/eventregistration/application/controller/ReviewORM.java
@@ -1,9 +1,16 @@
 package com.eventregistration.application.controller;
 
-import com.eventregistration.application.repository.OrganizerRepository;
+import com.eventregistration.application.model.Reviews;
 import com.eventregistration.application.repository.ReviewRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +23,27 @@ public class ReviewORM {
   ReviewRepository reviewRepository;
 
 
+  @GetMapping(value = "/event/{eventid}")
+  public List<Reviews> findAllReviewsByEvent(@PathVariable("eventid") Integer eventid) {
+    return reviewRepository.findReviewsByEvent(eventid);
+  }
+
+  @PostMapping(value = "/create")
+  public Reviews createReview(@RequestBody Reviews reviews) {
+    return reviewRepository.save(reviews);
+  }
+
+  @PutMapping(value = "/update/{revid}")
+  public Reviews updateReview(@PathVariable("revid") Integer revid, @RequestBody Reviews reviews) {
+    Reviews currentReview = reviewRepository.findReviewsById(revid);
+    currentReview.setScore(reviews.getScore());
+    currentReview.setComments(reviews.getComments());
+    return reviewRepository.save(currentReview);
+  }
+
+  @DeleteMapping(value = "/delete/{revid}")
+  public void removeReview(@PathVariable("revid") Integer revid) {
+    reviewRepository.deleteById(revid);
+  }
 
 }
--- src/main/java/com/eventregistration/application/repository/ReviewRepository.java
+++ src/main/java/com/eventregistration/application/repository/ReviewRepository.java
@@ -1,10 +1,18 @@
 package com.eventregistration.application.repository;
 
 import com.eventregistration.application.model.Reviews;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReviewRepository extends CrudRepository<Reviews, Integer> {
+
+  @Query(value = "SELECT * FROM reviews WHERE reviews.event_eventid = :id", nativeQuery = true)
+  public List<Reviews> findReviewsByEvent(Integer id);
+
+  @Query(value = "SELECT * FROM reviews WHERE reviews.review_id = :id", nativeQuery = true)
+  public Reviews findReviewsById(Integer id);
 
 }
