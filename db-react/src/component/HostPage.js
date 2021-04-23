ef75783fa7b0cf840757b0bfc9f0a1207d511076
EmreGucer <emreg2000@gmail.com>
2021-04-23 09:29:59 -0400

    Update on HostPage.js

--- db-react/src/component/HostPage.js
+++ db-react/src/component/HostPage.js
@@ -1,7 +1,10 @@
-import React, { Component } from "react";
-import HostService from "../service/HostService";
-import {Header, Input, Form, Button} from 'semantic-ui-react';
+import React, {Component} from "react";
+import OrganizerService from "../service/OrganizerService";
+import {Button, Form, Grid, Header, Card} from 'semantic-ui-react';
 import {Link} from "react-router-dom";
+import EventsService from "../service/EventsService";
+import HostService from "../service/HostService";
+
 
 class HostPage extends Component {
     constructor(props) {
@@ -11,19 +14,36 @@ class HostPage extends Component {
         this.saveHost = this.saveHost.bind(this);
         this.removeHost = this.removeHost.bind(this);
         this.getHost = this.getHost.bind(this);
+        this.getOrganizer = this.getOrganizer.bind(this);
+        this.getEvent = this.getEvent.bind(this);
+        this.retrieveAllEvent = this.retrieveAllEvent.bind(this);
+        this.retrieveAllOrganizer = this.retrieveAllOrganizer.bind(this);
 
         this.state = {
-            currentHost: {
-                hostId: null,
-                eventId: "",
-                organizerId: "",
-            }
+            organizers: [],
+            currentOrganizer : {
+            currentIndex: -1,
+            searchTitle: ""
+            },
+            currentEvent: {
+                eventid: null,
+                name: "",
+                type: "",
+                startdate: "",
+                enddate: "",
+                description: "",
+                fee: ""
+            },
+            event: [],
+            updated: false
         };
     }
 
     componentDidMount() {
         console.log(this.props.match.params.id);
         this.getHost(this.props.match.params.id);
+        this.retrieveAllEvent();
+        this.retrieveAllOrganizer();
     }
 
     onChangeEventId(e) {
@@ -102,49 +122,136 @@ class HostPage extends Component {
             });
     }
 
+
+    getEvent(id) {
+        EventsService.get(id)
+            .then(response => {
+                this.setState({
+                    currentEvent: response.data
+                });
+                console.log(this.state.currentEvent);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    }
+
+    getOrganizer(id) {
+        OrganizerService.get(id)
+            .then(response => {
+                this.setState({
+                    currentOrganizer: response.data
+                });
+                console.log(this.state.currentOrganizer);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    }
+
+    retrieveAllEvent() {
+        EventsService.getAll()
+            .then(response => {
+                this.setState({
+                    event: response.data
+                });
+                console.log(this.state.event);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    }
+
+    retrieveAllOrganizer() {
+        OrganizerService.getAll()
+            .then(response => {
+                this.setState({
+                    organizers: response.data
+                });
+                console.log(this.state.organizers);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    }
+
     render() {
-        const { currentHost } = this.state;
+        const {currentEvent, currentOrganizer} = this.state;
+
+        const EventOptions = this.state.event.map(data => ({
+            value: data.eventid,
+            text: data.name + " " + data.type
+        }))
+
+        const OrganizerOptions = this.state.organizers.map(data => ({
+            value: data.id,
+            text: data.firstName + " " + data.lastName
+        }))
+
 
         return (<div>
-                {currentHost ? (
+                {
                     <div className="edit-form">
-                        <Header as='h2'>Host</Header>
-                        <Form>
-                            <Form.Group widths='equal'>
-                                <Form.Input
-                                    label="Event-id"
-                                    type="text"
-                                    id= "null"
-                                    value={currentHost.eventId}
-                                    onChange={(e) => this.onChangeEventId(e)}
-                                />
-                                <Form.Input
-                                    label="Organizer id"
-                                    type="text"
-                                    id= "null"
-                                    value={currentHost.organizerId}
-                                    onChange={(e) => this.onChangeOrganizerId(e)}
-                                />
-                            </Form.Group>
+                        <Grid relaxed>
+                            <Grid.Row>
+                                <Header as='h2'>Host Sign Up</Header>
+                            </Grid.Row>
 
-                            <Form.Group>
+                            <Grid.Row>
+
+                                <Grid.Column width={6}>
+
+                                    <Form>
+                                        <Form.Select
+                                            fluid
+                                            label='Organizer'
+                                            options={OrganizerOptions}
+                                            value={currentOrganizer.organizerId}
+                                            placeholder='Select an organizer'
+                                            onChange={(e, {value}) => this.getOrganizer(value)}
+                                        />
+                                    </Form>{currentOrganizer.name &&
+
+                                <Card
+                                    fluid
+                                    link={"/event/find/" + currentEvent.eventid}
+                                    header={currentEvent.name + " " + currentEvent.type}
+                                    meta={currentEvent.startdate + "-" + currentEvent.enddate}
+                                    description={currentEvent.description}
+                                    extra={"Entry Fee: $" + currentEvent.fee}
+                                />}
+                                    <Form>
+                                        <Form.Select
+                                            fluid
+                                            label='Event'
+                                            options={EventOptions}
+                                            value={currentEvent.eventid}
+                                            placeholder='Select an Event'
+                                            onChange={(e, {value}) => this.getEvent(value)}
+                                        />
+                                    </Form>{currentEvent.name &&
+                                <Card
+                                    fluid
+                                    link={"/event/find/" + currentEvent.eventid}
+                                    header={currentEvent.name + " " + currentEvent.type}
+                                    meta={currentEvent.startdate + "-" + currentEvent.enddate}
+                                    description={currentEvent.description}
+                                    extra={"Entry Fee: $" + currentEvent.fee}
+                                />}
+                                </Grid.Column>
+                            </Grid.Row>
+                            <div>
                                 <Button
-                                    as={Link} to = "/"
+                                    primary
+                                    as={Link} to = {"/host/" + currentEvent.id + currentOrganizer}
                                     onClick={() => this.saveHost()}
-                                >Submit</Button>
-                                <Button
-                                    as={Link} to = "/"
-                                    onClick={() => this.removeHost()}
-                                >Delete</Button>
-                            </Form.Group>
-                        </Form>
+                                >
+                                    Register
+                                </Button>
+                            </div>
+                        </Grid>
                     </div>
-                ) : (
-                    <div>
-                        <br />
-                        <p>Please click on a Host...</p>
-                    </div>
-                )}
+                }
             </div>
         );
     }
